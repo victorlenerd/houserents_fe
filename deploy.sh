bf0be9c9706136e1b8f87f1869d6bc07970ec4d5
sleekvick <nvonweb@outlook.com>
2019-07-11 23:37:35 -0500

    Register authentication

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+$(aws ecr get-login --region us-east-2 --no-include-email)
 docker run -d -p 5000:5000  \
     --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-fe-develop:latest
