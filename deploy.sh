4cbd5a0c2988fa45ece0c551986a805c37b33efc
sleekvick <nvonweb@outlook.com>
2019-07-11 23:58:15 -0500

    Add envs to docker rub

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 $(aws ecr get-login --region us-east-2 --no-include-email)
-docker run -d -p 5000:5000  \
+docker run -d -p 4040:4040  \
+    -e PORT=4040 \
+    -e API_SERVER=http://0.0.0.0:5000 \
+    -e MAP_API_KEY=AIzaSyCp3UKASbZkqvCnW3l_RLgM5Ik15JBKpPc \
     --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-fe-develop:latest
