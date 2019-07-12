1e6adfbbf7a4c2397635df4873975e6bfe8f84ca
sleekvick <nvonweb@outlook.com>
2019-07-12 04:38:07 -0500

    Dp

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,4 @@ docker run -d -p 4040:4040  \
     -e PORT="4040" \
     -e API_SERVER="http://0.0.0.0:5000" \
     -e MAP_API_KEY="AIzaSyCp3UKASbZkqvCnW3l_RLgM5Ik15JBKpPc" \
-    --name	houserents-fe \
     --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-fe-develop:latest
