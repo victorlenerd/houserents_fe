e9529b9a1eefba460fb20f2ec167a5b13cc43a7c
sleekvick <nvonweb@outlook.com>
2019-07-12 05:13:28 -0500

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 $(aws ecr get-login --region us-east-2 --no-include-email)
 docker pull 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-fe-develop:latest
+docker stop houserents-fe
 docker container rm houserents-fe
 docker run -d -p 4040:4040  \
     --env PORT="4040" \
