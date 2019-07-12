3ad82516fab73c2d12ca4a265993f8108a5efec5
sleekvick <nvonweb@outlook.com>
2019-07-12 06:07:27 -0500

    Deploy

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 $(aws ecr get-login --region us-east-2 --no-include-email)
+docker rm $(docker ps -qf name=houserents-fe)
 docker run -d -p 4040:4040  \
     --env PORT="4040" \
     --env API_SERVER="http://0.0.0.0:5000" \
     --env MAP_API_KEY="AIzaSyCp3UKASbZkqvCnW3l_RLgM5Ik15JBKpPc" \
+    --name houserents-fe
     --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-fe-develop:latest
