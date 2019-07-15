7acc467be07f23ca5738bbb1f9ef3566cc06d821
sleekvick <nvonweb@outlook.com>
2019-07-15 12:56:14 -0500

    Remove unused files

--- deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker stop houserents-fe || true && docker rm houserents-fe || true
-$(aws ecr get-login --region us-east-2 --no-include-email)
-docker run -d -p 4040:4040 --name houserents-fe --rm 699011322781.dkr.ecr.us-east-2.amazonaws.com/houserents-fe-develop:latest
