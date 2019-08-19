76840d403ca7dfc36a4c5400abec2ed4d50c9182
Nwaokocha Victor <nvonweb@outlook.com>
2019-08-19 06:40:56 -0500

    Pagination component

--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,7 +1,28 @@
-import { useState, useEffect } from 'react';
+import * as React from 'react';
+import './index.css';
 
-const Pagination = () => {
-    const [] = useState();
+interface IProps {
+    total: number
+    itemsPerPage: number
+    onChange: (page: number) => void
+}
+
+const Pagination: React.FunctionComponent<IProps> = ({ total, itemsPerPage, onChange }) => {
+    const pageCounts = Math.ceil(total / itemsPerPage);
+    const pageNumbers = [];
+
+    for (let i = 0; i < pageCounts; i ++) {
+        pageNumbers.push(i + 1);
+    }
+
+    return (
+        <div className="pagination-container">
+            <select data-testid="pagination-select"  onChange={(e) => onChange(Number(e.target.value))}>
+                {pageNumbers.length > 0 && pageNumbers.map((pageNumber, index) => <option key={index} value={pageNumber}>{pageNumber}</option>)}
+            </select>
+            <div className="pagination-of">of {pageCounts}</div>
+        </div>
+    );
 };
 
 export default Pagination;
